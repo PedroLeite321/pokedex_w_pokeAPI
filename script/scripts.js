be931eabf345dcf9b6b6340c3cb301fb3fa0ec24
pedro <pk.bleite@gmail.com>
2023-12-16 23:18:29 -0300

    stupid code

--- script/scripts.js
+++ script/scripts.js
@@ -7,31 +7,44 @@ const pokedex = (() =>   {
 
 
     const fetchPkm = async (pokemon) =>  {
-        const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
-        const data = await APIResponse.json();
-        console.log(data)
-        return data;
+        console.log(pokemon);
+        const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+       if(APIResponse.status === 200)   {
+            const data = await APIResponse.json();
+            return data;
+       }
     }
 
     const renderPkm = async (pokemon) =>  {
         const data = await fetchPkm(pokemon); //will receive data from fetchPkm]
-        console.log(data);
-        pokemonName.innerHTML = data.name;
-        pokemonNumber.innerHTML = data.id;
-        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        if(data)    {
+            pokemonName.innerHTML = data.name;
+            pokemonNumber.innerHTML = data.id;
+            pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        }else{
+            pokemonName.innerHTML = "NOT FOUND T-T";
+        }
+       
+        const nextBtn = document.querySelector('.pokedex-next').addEventListener('click', async () => {
+            
+            await renderPkm(data.id += 1);
+        })
+        const prevBtn = document.querySelector('.pokedex-prev').addEventListener('click', async () => {
+           await renderPkm(data.id -= 1);
+        })
     }
     
     const confirmBtn = document.querySelector('.pokedex-confirm').addEventListener('click', (search) => {
             
-        if(pokemonInput.value === null || pokemonInput.value === undefined || pokemonInput.value === '')    {
+        if(pokemonInput.value.toLowerCase() === null || pokemonInput.value === undefined || pokemonInput.value === '')    {
             alert("Insira o nome ou endereço da pokédex de um pokémon");
         }else{
-        renderPkm(pokemonInput.value);
+        renderPkm(pokemonInput.value.toLowerCase());
 
     }});
     const form = document.querySelector('.form').addEventListener('submit', (search) => {
 
         search.preventDefault()
-        renderPkm(pokemonInput.value);
+        renderPkm(pokemonInput.value.toLowerCase());
     });
 })();
